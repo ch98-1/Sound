d3979e4c6eb0a7a2b4ccd794031c605810c24cb4
ch98-1 <akerunakayama+1@gmail.com>
2015-04-30 09:51:58 -0700

    Created big endian test

--- main.c
+++ main.c
@@ -1,35 +1,32 @@
 //Copyright (C) 2015  Corwin Hansen
-#include "main.c"
+#include "main.h"
 
 int main(int argc, char *argv[]){
 	if (argc != 3){//if there was not correct number of argument
 		printf("Plese input file to compile and destination file");
 		exit(EXIT_SUCCESS);//exit software
 	}
-
+	printf("%u", IntBigEndian());
 	exit(EXIT_SUCCESS);//exit software
 }
 
 int IntBigEndian(void){//get if integer is in big endian
+	union {
+		uint32_t l;
+		uint8_t c[4];
+	} test = { 0x01020304 };
+
+	return test.c[0] == 0x01;
+}
+
+int WriteInt_16(FILE *file, int16_t data){//write data to file in little endian. return 0 on succuess
 
 }
 
-int FloatBigEndian(void){//get if float is in big endian
+int WriteInt_32(FILE *file, int32_t data){//write data to file in little endian. return 0 on success
 
 }
 
-int WriteInt_16(FILE *file, uint16_t data){//write data to file in little endian. return 0 on succuess
-
-}
-
-int WriteInt_32(FILE *file, uint32_t data){//write data to file in little endian. return 0 on success
-
-}
-
-int WriteFloat(FILE *file, float data){//write data to file in little endian. return 0 on success
-
-}
-
-int WriteWav(FILE *file, float *data, uint32_t size){//write wav file with data of size elements. return 0 on success
+int WriteWav(FILE *file, int32_t *data, uint32_t size){//write wav file with data of size. return 0 on success
 
 }
--- main.h
+++ main.h
@@ -5,9 +5,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
+#include <stdint.h>
 
-
+#define FORMAT (0xfffe)
+#define SUB_FORMAT (0x0001)
+#define BITRATE 192000
 
 
 
@@ -17,11 +19,9 @@
 
 
 int IntBigEndian(void);//get if integer is in big endian
-int FloatBigEndian(void);//get if float is in big endian
-int WriteInt_16(FILE *file, uint16_t data);//write data to file in little endian. return 0 on succuess
-int WriteInt_32(FILE *file, uint32_t data);//write data to file in little endian. return 0 on success
-int WriteFloat(FILE *file, float data);//write data to file in little endian. return 0 on success
-int WriteWav(FILE *file, float *data, uint32_t size);//write wav file with data of size elements. return 0 on success
+int WriteInt_16(FILE *file, int16_t data);//write data to file in little endian. return 0 on succuess
+int WriteInt_32(FILE *file, int32_t data);//write data to file in little endian. return 0 on success
+int WriteWav(FILE *file, int32_t *data, uint32_t size);//write wav file with data of size elements. return 0 on success
 
 
 #endif
